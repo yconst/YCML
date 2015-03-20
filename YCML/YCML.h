13874e88f4a0ba03dd5fda4a173c3fe987679a23
Ioannis Chatzikonstantinou <contact@yconst.com>
2015-03-20 14:49:30 +0200

    Updated copyright notice

--- YCML/YCML.h
+++ YCML/YCML.h
@@ -5,6 +5,8 @@
 //  Created by Ioannis (Yannis) Chatzikonstantinou on 2/3/15.
 //  Copyright (c) 2015 Ioannis (Yannis) Chatzikonstantinou. All rights reserved.
 //
+// This file is part of YCML.
+//
 // YCML is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
