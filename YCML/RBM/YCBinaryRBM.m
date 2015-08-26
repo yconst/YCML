82fb0d4ff102205a56c71c3bacc46850e6d06982
Ioannis Chatzikonstantinou <contact@yconst.com>
2015-08-26 15:59:24 +0300

    Updated copyright notices

--- YCML/RBM/YCBinaryRBM.m
+++ YCML/RBM/YCBinaryRBM.m
@@ -2,9 +2,23 @@
 //  YCBinaryRBM.m
 //  YCML
 //
-//  Created by Ioannis Chatzikonstantinou on 30/7/15.
-//  Copyright © 2015 Yannis Chatzikonstantinou. All rights reserved.
+//  Created by Ioannis (Yannis) Chatzikonstantinou on 2/3/15.
+//  Copyright (c) 2015 Ioannis (Yannis) Chatzikonstantinou. All rights reserved.
 //
+// This file is part of YCML.
+//
+// YCML is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// YCML is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with YCML.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "YCBinaryRBM.h"
 
